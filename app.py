1a1d7a06ff28978b1af94b9440586636ee4bba86
codebleeder <sharad.shivmath@gmail.com>
2015-12-14 17:46:07 -0800

    Added winning moves check to Board class

--- app.py
+++ app.py
@@ -6,11 +6,34 @@ if __name__ == '__main__':
 
 	symbols = get_human_symbol()
 	b = Board(symbols['human_symbol'], symbols['computer_symbol'])
-	
+	'''
 	while b.is_board_full() is False:
 		b.print_board()
 		cell_num = get_cell_num()
 		while b.make_human_entry(cell_num) is False:
 			print 'The cell is occupied!'
 			cell_num = get_cell_num()
+	'''
+
+	'''
+	# vertical check
+	b.make_human_entry(1)
+	b.make_human_entry(5)
+	b.make_human_entry(7)
 	
+	b._Board__make_computer_entry(2)
+	b._Board__make_computer_entry(8)
+	b.print_board()
+	print b._Board__check_for_winning_move()
+	'''
+
+	# diagonal check
+	b.make_human_entry(2)
+	b.make_human_entry(5)
+	b.make_human_entry(7)
+	
+	b._Board__make_computer_entry(2)
+	b._Board__make_computer_entry(8)
+	b.print_board()
+	print b._Board__check_for_winning_move()
+			
--- board.py
+++ board.py
@@ -32,5 +32,45 @@ class Board:
 		else:
 			return True 
 
-	def make_computer_entry(self):
+	
+
+	def __check_for_winning_move(self):
+		# Horizontal check
+		for i in range(0, 7, 3):
+			horizontal_triplet = self.arr[0+i:3+i]
+			if horizontal_triplet.count(self.computer_symbol) == 2 and horizontal_triplet.count(' ') == 1:
+				return {'winning_move_present': True, 'index': self.arr[0+i:3+i].index(' ')+i}
 		
+
+		# Vertical check
+		for j in range(0, 3):
+			vertical_triplet = [self.arr[j], self.arr[j+3], self.arr[j+6]]
+			if vertical_triplet.count(self.computer_symbol) == 2 and vertical_triplet.count(' ') == 1:
+				return {'winning_move_present': True, 'index' : vertical_triplet.index(' ')*3+j}
+
+		
+
+		# diagonal check
+		diagonal_triplet1 = [self.arr[0], self.arr[4], self.arr[8]]
+		if diagonal_triplet1.count(self.computer_symbol) == 2 and diagonal_triplet1.count(' ') == 1:
+			return {'winning_move_present': True, 'index' : diagonal_triplet1.index(' ')*4}
+		diagonal_triplet2 = [self.arr[2], self.arr[4], self.arr[6]]
+		if diagonal_triplet2.count(self.computer_symbol) == 2 and diagonal_triplet2.count(' ') == 1:
+			return {'winning_move_present': True, 'index' : diagonal_triplet1.index(' ')*4}
+		return {'winning_move_present': False, 'index': 0}
+		
+	'''
+	def __make_computer_entry(self, index):
+		check = __check_for_winning_move()
+		if check['winning_move_present'] == True:
+			self.arr[check[index]] = computer_symbol
+		else:
+			print 'winning move not present'
+	'''
+
+	def __make_computer_entry(self, index):
+		self.arr[index] = self.computer_symbol
+
+
+
+
